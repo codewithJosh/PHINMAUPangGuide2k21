4088a13b67cfcb2386ace9c3e805b9924984838d
codewithJosh <dacasinjoshua21@gmail.com>
2022-04-21 23:10:15 +0800

    feat: add `back` button

--- app/src/main/java/com/codewithjosh/PHINMAUPangGuide2k21/EnrollmentActivity.java
+++ app/src/main/java/com/codewithjosh/PHINMAUPangGuide2k21/EnrollmentActivity.java
@@ -1,14 +1,27 @@
 package com.codewithjosh.PHINMAUPangGuide2k21;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
 public class EnrollmentActivity extends AppCompatActivity {
+
+    ImageButton _backBtn;
+    TextView _linkEnroll, _linkProof;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enrollment);
+
+        _backBtn = findViewById(R.id.btn_return);
+        _linkEnroll =  findViewById(R.id.link_enroll);
+        _linkProof =  findViewById(R.id.link_proof);
+
+        _backBtn.setOnClickListener(view -> onBackPressed());
+
     }
+
 }
