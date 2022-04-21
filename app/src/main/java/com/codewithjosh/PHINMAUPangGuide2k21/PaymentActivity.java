9841670ae852b908879bb9446388945ca82f63ed
codewithJosh <dacasinjoshua21@gmail.com>
2022-04-22 01:13:37 +0800

    refactor: update `payment` activity

--- app/src/main/java/com/codewithjosh/PHINMAUPangGuide2k21/PaymentActivity.java
+++ app/src/main/java/com/codewithjosh/PHINMAUPangGuide2k21/PaymentActivity.java
@@ -1,22 +1,23 @@
 package com.codewithjosh.PHINMAUPangGuide2k21;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.widget.ImageButton;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 public class PaymentActivity extends AppCompatActivity {
 
-    ImageButton _backBtn;
+    ImageButton btn_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
 
-        _backBtn = findViewById(R.id.btn_back);
+        btn_back = findViewById(R.id.btn_back);
 
-        _backBtn.setOnClickListener(view -> onBackPressed());
+        btn_back.setOnClickListener(v -> onBackPressed());
 
     }
+
 }
