6f9a359813d94a33a758ca90aa50fea92cb8b0e0
codewithJosh <dacasinjoshua21@gmail.com>
2022-04-21 23:34:01 +0800

    refactor: finishing touches

--- app/src/main/java/com/codewithjosh/PHINMAUPangGuide2k21/PaymentActivity.java
+++ app/src/main/java/com/codewithjosh/PHINMAUPangGuide2k21/PaymentActivity.java
@@ -1,9 +1,9 @@
 package com.codewithjosh.PHINMAUPangGuide2k21;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class PaymentActivity extends AppCompatActivity {
 
