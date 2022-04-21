32a3e143208cdfbf15d5074c205eefd392157652
codewithJosh <dacasinjoshua21@gmail.com>
2022-04-21 23:33:25 +0800

    feat: add `back` button

--- app/src/main/java/com/codewithjosh/PHINMAUPangGuide2k21/PaymentActivity.java
+++ app/src/main/java/com/codewithjosh/PHINMAUPangGuide2k21/PaymentActivity.java
@@ -1,14 +1,22 @@
 package com.codewithjosh.PHINMAUPangGuide2k21;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+import android.widget.ImageButton;
+
 public class PaymentActivity extends AppCompatActivity {
+
+    ImageButton _backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
+
+        _backBtn = findViewById(R.id.btn_return);
+
+        _backBtn.setOnClickListener(view -> onBackPressed());
+
     }
 }
