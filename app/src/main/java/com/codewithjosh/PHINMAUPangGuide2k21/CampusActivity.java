09b01d547233a4aaf4540ba2a50acf6a51c40ee5
codewithJosh <dacasinjoshua21@gmail.com>
2022-04-21 23:41:17 +0800

    feat: add `back` button

--- app/src/main/java/com/codewithjosh/PHINMAUPangGuide2k21/CampusActivity.java
+++ app/src/main/java/com/codewithjosh/PHINMAUPangGuide2k21/CampusActivity.java
@@ -1,14 +1,26 @@
 package com.codewithjosh.PHINMAUPangGuide2k21;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class CampusActivity extends AppCompatActivity {
+
+    ImageButton _backBtn;
+    RecyclerView _recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_campus);
+
+        _backBtn = findViewById(R.id.btn_return);
+        _recyclerView = findViewById(R.id.campusRecyclerView);
+
+        _backBtn.setOnClickListener(view -> onBackPressed());
+
     }
+
 }
